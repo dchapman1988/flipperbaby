6749e7d15fa8ea451f4527c70c89a7bbb9c97d91
David Chapman <david@isotope11.com>
2013-08-28 00:30:42 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flipperbaby::Application.config.secret_key_base = 'de808b07d33f53a6fe4c84b743d0c4c92b15433d07a25a3f98dc0c3d08a49f84281f81f1aac41bb87e03b7a8ce5c1c47394370df19829a76483403b04fa0de6e'
